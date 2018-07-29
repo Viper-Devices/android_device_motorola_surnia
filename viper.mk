c0c715375de9734aab54c340255b63848e34db39
EsromTech <esromgalaxy5@gmail.com>
2018-07-29 06:59:23 -0300

    ViperOS-ify

--- viper.mk
+++ viper.mk
@@ -19,9 +19,12 @@ $(call inherit-product, device/motorola/surnia/full_surnia.mk)
 TARGET_SCREEN_WIDTH := 540
 TARGET_SCREEN_HEIGHT := 960
 
+# Build type
+VIPER_BUILD_TYPE=OFFICIAL
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := surnia
-PRODUCT_NAME := lineage_surnia
+PRODUCT_NAME := viper_surnia
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := surnia
