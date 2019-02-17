0c6f5e320ab4381e09945261360f9ca13ec1788e
EsromTech <esromgalaxy5@gmail.com>
2019-02-17 07:58:40 -0300

    ViperOS BringUp

--- viper_surnia.mk
+++ viper_surnia.mk
@@ -22,10 +22,8 @@ TARGET_SCREEN_HEIGHT := 960
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := surnia
-PRODUCT_NAME := lineage_surnia
+PRODUCT_NAME := viper_surnia
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := surnia
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="Moto E LTE (2nd gen)"
+PRODUCT_MODEL := Moto E LTE (2nd gen)
